45cd134af3e2e3e581d2bacdee01ada17ba340c8
Long Le <longle1@illinois.edu>
2014-04-18 18:35:49 -0500

    add featExtract.m subroutine. This is used in main.m

---
+++ featExtract.m
@@ -0,0 +1,39 @@
+function [d, p, f] = featExtract(img, blobSizeThresh)
+% Feature extraction using vl_covdet with blob size 'adjustment'
+%
+% Input
+%   img - input image
+% Output
+%   d - descriptor  % 128 x n
+%   p - raw patch   % 1681 x n
+%   f - frame       % 6 x n
+%
+% Long Le
+% University of Illinois
+%
+
+im = single(rgb2gray(img)); % Shouldn't use imshow to display this
+[f, d] = vl_covdet(im, 'EstimateOrientation', true, 'EstimateAffineShape', true, 'PeakThreshold', 5, 'EdgeThreshold', 5);
+[~, p] = vl_covdet(im, 'EstimateOrientation', true, 'EstimateAffineShape', true, 'PeakThreshold', 5, 'EdgeThreshold', 5, 'descriptor', 'Patch');
+
+% Screen out too small blobs
+suppress = false(1, size(f, 2));
+for l = 1:size(f, 2)
+    tmp = f(:,l);
+    % scales of the affine xformation matrix A, see oriented
+    % ellipse and vl_demo_frame.m for details
+    tmp = svd([tmp(3) tmp(5); tmp(4) tmp(6)]);
+    if (max(tmp) < blobSizeThresh)
+        suppress(l) = true;
+    end
+end
+f(:, suppress) = [];
+d(:, suppress) = [];
+p(:, suppress) = [];
+
+% Display
+%{
+figure; imshow(img);
+h = vl_plotframe(f);
+set(h,'color','y','linewidth',2);
+%}
--- main.m
+++ main.m
@@ -18,51 +18,26 @@ for k = 1:nFrame
 end
 %}
 
-folder = 'testim';
-fnames = dir(fullfile(folder, '*.jpg'));
-nFrame = length(fnames);
+folder = 'imTrain';
+file = dir(fullfile(folder, '*.jpg'));
+nFrame = length(file);
 img = cell(1, nFrame);
 for k = 1:nFrame
-    img{k} = imread(fullfile(folder, fnames(k).name));
+    img{k} = imread(fullfile(folder, file(k).name));
 end
 
-%% Feature detection and description
+%% Feature extraction for training images
 blobSizeThresh = 10;
 
-im = cell(1, nFrame);
-f = cell(1, nFrame); % frame
 d = cell(1, nFrame); % descriptor
 p = cell(1, nFrame); % raw patch
 for k = 1:nFrame
-    im{k} = single(rgb2gray(img{k})); % Shouldn't use imshow to display this
-    [f{k}, d{k}] = vl_covdet(im{k}, 'EstimateOrientation', true, 'EstimateAffineShape', true, 'PeakThreshold', 5, 'EdgeThreshold', 5);
-    [~, p{k}] = vl_covdet(im{k}, 'EstimateOrientation', true, 'EstimateAffineShape', true, 'PeakThreshold', 5, 'EdgeThreshold', 5, 'descriptor', 'Patch');
-    
-    % Screen out too small blobs
-    suppress = false(1, size(f{k}, 2));
-    for l = 1:size(f{k}, 2)
-        tmp = f{k}(:,l);
-        % scales of the affine xformation matrix A, see oriented
-        % ellipse and vl_demo_frame.m for details
-        tmp = svd([tmp(3) tmp(5); tmp(4) tmp(6)]);
-        if (max(tmp) < blobSizeThresh)
-            suppress(l) = true;
-        end
-    end
-    f{k}(:, suppress) = [];
-    d{k}(:, suppress) = [];
-    p{k}(:, suppress) = [];
-    
-    % Display
-    figure; imshow(img{k});
-    h = vl_plotframe(f{k});
-    set(h,'color','y','linewidth',2);
+    [d{k}, p{k}] = featExtract(img{k}, blobSizeThresh);
 end
 
-%% Clustering
-F = cell2mat(f); % 6 x n
-D = cell2mat(d); % 128 x n
-P = cell2mat(p); % 1681 x n
+%% Clustering to find visual dictionary
+D = cell2mat(d);
+P = cell2mat(p); 
 % Debugging
 %{
 for k = 1:10
@@ -74,28 +49,33 @@ end
 
 K = 32;
 for k = 1:nFrame
-    [C, A]= vl_kmeans(double(D), K, 'NumRepetitions', 10);
+    [C, A]= vl_kmeans(single(D), K, 'NumRepetitions', 10);
 end
 % Debugging
 figure; hist(double(A), K);
 
 % Visual words/dictionary
-vwordF = cell(1, K);
 vwordD = cell(1, K);
 vwordP = cell(1, K);
 for k = 1:K
-    vwordF{k} = F(:, A == k);
     vwordD{k} = D(:, A == k);
     vwordP{k} = P(:, A == k);
 end
 % Debugging
+%{
 figure;
 for k = 1:size(vwordP{15}, 2)
     imPatch = reshape(vwordP{15}(:,k), 41,41);
     subplot(1, size(vwordP{15}, 2), k); imagesc(imPatch)
 end
+%}
 
 %% Compute frequency vector for all the frame
-for k = 1:nFrame
-    
-end
+% Extract features from a test image
+file = dir(fullfile('imTest', '*.jpg'));
+imgTest = imread(fullfile('imTest', file.name));
+[dTest, pTest] = featExtract(imgTest, blobSizeThresh);
+
+% Compute distance between sift descriptor using L2 norm
+freqVec = zeros(1, K);
+distMat = vl_alldist2(dTest, C);
