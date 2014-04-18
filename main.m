5237dfde5420bea6361c82503f431c95768d6a26
Long Le <longle1@illinois.edu>
2014-04-17 19:48:59 -0500

    clustering done.

--- main.m
+++ main.m
@@ -26,26 +26,35 @@ for k = 1:nFrame
 end
 
 %% Run blob detection on each of the 48 frame
+im = cell(1, nFrame);
 f = cell(1, nFrame);
 d = cell(1, nFrame);
 for k = 1:nFrame
-    figure; imshow(img{k});
-    im = single(rgb2gray(img{k}));
+    im{k} = single(rgb2gray(img{k}));
     [f{k}, d{k}] = vl_sift(im, 'WindowSize', 4, 'NormThresh', 5, 'PeakThresh', 5, 'EdgeThresh', 10);
-    h = vl_plotframe(f{k});
-    set(h,'color','y','linewidth',2);
+    %figure; imshow(img{k});
+    %h = vl_plotframe(f{k});
+    %set(h,'color','y','linewidth',2);
 end
 
 %% Clustering
-F = cell2mat(f)';
-D = cell2mat(d)';
+F = cell2mat(f);
+D = double(cell2mat(d));
 
-K = 128;
+K = 32;
 for k = 1:nFrame
-    [idx, C]= kmeans(D, K, 'replicates', 10);
+    [C, A]= vl_kmeans(D, K, 'NumRepetitions', 10);
 end
 
-word = cell(1, K);
+vword = cell(1, K); % Visual words/dictionary
 for k = 1:K
+    vword{k} = F(:, A == k);
+end
+
+% Debug by plotting all the image patches
+vword
+
+%% Compute frequency vector for all the frame
+for k = 1:nFrame
     
 end
