14ba69976dc3371437da39e441e3cc46188e3242
Long Le <longle1@illinois.edu>
2014-05-08 22:38:04 -0500

    first step into the final demo: find the top 5 revelance given a test image

--- main.m
+++ main.m
@@ -8,16 +8,6 @@ clear all; close all;
 %run('../../vlfeat/toolbox/vl_setup')
 
 %% Very specific scene for this video.
-%{
-vid = VideoReader('charade.mp4');
-frameIdx = 1;%:500:vid.NumberOfFrames; % frame index
-nFrame = numel(frameIdx);
-img = cell(1, nFrame);
-for k = 1:nFrame
-    img{k} = read(vid, frameIdx(k));
-end
-%}
-
 folder = 'imTrain';
 file = dir(fullfile(folder, '*.jpg'));
 nFrame = length(file);
@@ -38,21 +28,11 @@ end
 %% Clustering to find visual dictionary
 D = cell2mat(d);
 P = cell2mat(p); 
-% Debugging
-%{
-for k = 1:10
-    figure;
-    imPatch = reshape(P(:,k), 41,41);
-    imagesc(imPatch);
-end
-%}
 
 K = 32;
 for k = 1:nFrame
     [C, A]= vl_kmeans(single(D), K, 'NumRepetitions', 10);
 end
-% Debugging
-figure; hist(double(A), K);
 
 % Form visual dictionary
 vdictD = cell(1, K);
@@ -63,14 +43,15 @@ for k = 1:K
 end
 % Debugging
 %{
+figure; hist(double(A), K);
 figure;
-for k = 1:size(vwordP{15}, 2)
-    imPatch = reshape(vwordP{15}(:,k), 41,41);
-    subplot(1, size(vwordP{15}, 2), k); imagesc(imPatch)
+for k = 1:5%size(vdictP{15}, 2)
+    imPatch = reshape(vdictP{1}(:,k), 41,41);
+    subplot(1, 5, k); imagesc(imPatch)
 end
 %}
 
-% Compute frequency vector for all training frame
+% Compute frequency vectors for all training frames
 freqVec = zeros(nFrame, K);
 for k = 1:nFrame
     distMat = vl_alldist2(d{k}, C);
@@ -78,16 +59,31 @@ for k = 1:nFrame
     freqVec(k,:) = hist(idx, K);
 end
 
-%% Compute frequency vector for all test frame
+save vdict.mat vdictD vdictP
+
+%% Compute frequency vector for a test frame
 % Extract features from a test image
 file = dir(fullfile('imTest', '*.jpg'));
-imgTest = imread(fullfile('imTest', file.name));
+imgTest = imread(fullfile('imTest', file(10).name));
 [dTest, pTest] = featExtract(imgTest, blobSizeThresh, true);
 
 % Compute distance between sift descriptor using L2 norm
 distMat = vl_alldist2(dTest, C);
 [~, idx] = min(distMat, [], 2);
 freqVecTest = hist(idx, K);
+xlabel('Visual word');
+ylabel('Count');
 
 % Find the most similar image from the training dataset
 score = vl_alldist2(freqVecTest', freqVec', 'HELL');
+[sortScore, frameIdx] = sort(score);
+
+% Display the top N most similar images
+N = 5;
+figure;
+set(gcf, 'units','normalized', 'position', [0 0 1 1])
+for k = 1:N
+    subplot(1,N,k); imshow(img{frameIdx(k)})
+    xlabel(sprintf('Relevance: %0.1f, Frame index: %d', 100-sortScore(k), frameIdx(k)))
+    %set(get(gca,'YLabel'),'Rotation',0)
+end
